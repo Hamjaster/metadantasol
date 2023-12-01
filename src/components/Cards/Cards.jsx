5a6abb9310419507ee43268f13562312c548bbc6
Hamjaster <hamzasepal@gmail.com>
2023-12-01 21:06:20 +0500

    fic

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Cards.css'
-import { Tilt } from 'react-tilt'
-import { FaComputer } from 'react-icons/fa6'
-import { HiDatabase } from "react-icons/hi";
-import { RiRobot2Fill } from "react-icons/ri";
-import { SiPaloaltosoftware } from "react-icons/si";
+import one from '../../images/one.mp4'
+import two from '../../images/two.mp4'
+import three from '../../images/three.mp4'
+import four from '../../images/four.mp4'
+
 export const Cards = () => {
   return (
 
@@ -14,17 +14,25 @@ export const Cards = () => {
         <div className=" mx-auto w-full">
           <h1 className="title text-5xl sm:text-6xl font-bold w-full py-3 text-[#444fff]">Our Solutions</h1>
 
-          <p className=' mt-3 text-lg text-gray-500 mx-24 '>At Metadatan Solutions, we bring over a decade of unwavering dedication to the BI and analytics sector, and we've embarked on a mission to leverage our extensive experience to support your journey to success. Our core mission revolves around empowering your management with the essential BI tools, ensuring a seamless voyage toward data-driven decision-making.</p>
+          <p className=' mt-3 text-lg text-gray-500 mx-7 sm:mx-24 '>At Metadatan Solutions, we bring over a decade of unwavering dedication to the BI and analytics sector, and we've embarked on a mission to leverage our extensive experience to support your journey to success. Our core mission revolves around empowering your management with the essential BI tools, ensuring a seamless voyage toward data-driven decision-making.</p>
 
           <div className="grid grid-cols-1 sm:grid-cols-2 w-10/12 mx-auto gap-6 my-16 ">
 
 
-            <div className="group w-full mx-auto rounded-sm bg-[#cee0ff] text-[#57647c] flex flex-col space-y-4 py-6 items-center border-none hover:bg-[#444fff] hover:text-[#444fff] cursor-pointer transition-all duration-500">
+            <div className="group pb-7 w-full mx-auto rounded-sm bg-[#cee0ff] text-[#57647c] flex flex-col space-y-4 items-center border-none hover:bg-[#444fff] hover:text-[#444fff] cursor-pointer transition-all duration-500">
 
-              <div className="img text-6xl  bg-[#9097ff] text-white p-6 rounded-full">
-                <FaComputer />
+              <div className="img relative overflow-hidden w-full h-72  ">
+                <video
+                  className="absolute top-0 left-0 z-0 w-full h-full object-cover"
+                  autoPlay
+                  loop
+                  muted
+                >
+                  <source src={one} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
               </div>
-              <div className="name text-black group-hover:text-white uppercase w-3/4 font-extrabold text-xl text-center">
+              <div className="name pt-3 text-black group-hover:text-white uppercase w-3/4 font-extrabold text-xl text-center">
                 <div>Business Intelligence Solutions</div>
               </div>
               <div className="content font-normal text-center group-hover:text-white text-base mx-3">
@@ -47,12 +55,20 @@ export const Cards = () => {
 
             </div>
 
-            <div className="group w-full mx-auto rounded-sm bg-[#cee0ff] text-[#57647c] flex flex-col space-y-4 py-6 items-center border-none hover:bg-[#444fff] hover:text-[#444fff] cursor-pointer transition-all duration-500">
+            <div className="group pb-7 w-full mx-auto rounded-sm bg-[#cee0ff] text-[#57647c] flex flex-col space-y-4 items-center border-none hover:bg-[#444fff] hover:text-[#444fff] cursor-pointer transition-all duration-500">
 
-              <div className="img text-6xl  bg-[#9097ff] text-white p-6 rounded-full">
-                <HiDatabase />
+              <div className="img relative overflow-hidden w-full h-72  ">
+                <video
+                  className="absolute top-0 left-0 z-0 w-full h-full object-cover"
+                  autoPlay
+                  loop
+                  muted
+                >
+                  <source src={two} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
               </div>
-              <div className="name text-black group-hover:text-white uppercase w-3/4 font-extrabold text-xl text-center">
+              <div className="name pt-3 text-black group-hover:text-white uppercase w-3/4 font-extrabold text-xl text-center">
                 <div>Data Visualization Services</div>
               </div>
               <div className="content font-normal text-center group-hover:text-white text-base mx-3">
@@ -76,12 +92,20 @@ export const Cards = () => {
 
             </div>
 
-            <div className="group w-full mx-auto rounded-sm bg-[#cee0ff] text-[#57647c] flex flex-col space-y-4 py-6 items-center border-none hover:bg-[#444fff] hover:text-[#444fff] cursor-pointer transition-all duration-500">
+            <div className="group pb-7 w-full mx-auto rounded-sm bg-[#cee0ff] text-[#57647c] flex flex-col space-y-4 items-center border-none hover:bg-[#444fff] hover:text-[#444fff] cursor-pointer transition-all duration-500">
 
-              <div className="img text-6xl  bg-[#9097ff] text-white p-6 rounded-full">
-                <RiRobot2Fill />
+              <div className="img relative overflow-hidden w-full h-72  ">
+                <video
+                  className="absolute top-0 left-0 z-0 w-full h-full object-cover"
+                  autoPlay
+                  loop
+                  muted
+                >
+                  <source src={three} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
               </div>
-              <div className="name text-black group-hover:text-white uppercase w-3/4 font-extrabold text-xl text-center">
+              <div className="name pt-3 text-black group-hover:text-white uppercase w-3/4 font-extrabold text-xl text-center">
                 <div>Data Science</div>
               </div>
               <div className="content font-normal text-center group-hover:text-white text-base mx-3">
@@ -103,12 +127,20 @@ export const Cards = () => {
 
             </div>
 
-            <div className="group w-full mx-auto rounded-sm bg-[#cee0ff] text-[#57647c] flex flex-col space-y-4 py-6 items-center border-none hover:bg-[#444fff] hover:text-[#444fff] cursor-pointer transition-all duration-500">
+            <div className="group pb-7 w-full mx-auto rounded-sm bg-[#cee0ff] text-[#57647c] flex flex-col space-y-4 items-center border-none hover:bg-[#444fff] hover:text-[#444fff] cursor-pointer transition-all duration-500">
 
-              <div className="img text-6xl  bg-[#9097ff] text-white p-6 rounded-full">
-                <SiPaloaltosoftware />
+              <div className="img relative overflow-hidden w-full h-72  ">
+                <video
+                  className="absolute top-0 left-0 z-0 w-full h-full object-cover"
+                  autoPlay
+                  loop
+                  muted
+                >
+                  <source src={four} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
               </div>
-              <div className="name text-black group-hover:text-white uppercase w-3/4 font-extrabold text-xl text-center">
+              <div className="name pt-3 text-black group-hover:text-white uppercase w-3/4 font-extrabold text-xl text-center">
                 <div>Custom Software Development</div>
               </div>
               <div className="content font-normal text-center group-hover:text-white text-base mx-3">
--- src/components/HeroSection/Hero.jsx
+++ src/components/HeroSection/Hero.jsx
@@ -6,9 +6,9 @@ import videoBg from '../../images/video.mp4'
 
 export const Hero = () => {
   return (
-    <>
+    <div>
 
-      <div className="video-container w-full h-screen absolute -z-10">
+      <div className="video-container h-[70vh] w-full absolute  -z-10">
         <video className='w-full h-full absolute object-cover' src={videoBg} autoPlay muted loop></video>
         <div className="overlay absolute top-0 left-0 w-full h-full bg-gradient-to-b from-[#000dc0] to-transparent opacity-80"></div>
       </div>
@@ -18,6 +18,6 @@ export const Hero = () => {
         <Heroleft />
         {/* <HeroRight /> */}
       </div>
-    </>
+    </div>
   )
 }
